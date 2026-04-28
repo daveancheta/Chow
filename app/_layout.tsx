6cbf910c75b14e74daa65965fad94f779f97ff4c
daveancheta <daveancheta8@gmail.com>
2026-04-28 19:11:49 +0800

    feat: enhance styling and theming with Tailwind CSS variables, update metro config for inline rem, and add new dependencies for improved UI components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,23 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar"; 
 import "../global.css"
+import { ThemeProvider } from "@react-navigation/native";
+import { Text, useColorScheme, View } from "react-native";
+import { Portal } from '@rn-primitives/portal';
+import { NAV_THEME } from "@/lib/theme";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  const colorScheme = useColorScheme() ?? "light";
+
+  return (
+    <ThemeProvider value={NAV_THEME[colorScheme]}>
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+      <Stack />
+      <Portal name='example-portal' hostName='example-host'>
+        <View>
+          <Text>I will be rendered above the Content component</Text>
+        </View>
+      </Portal>
+    </ThemeProvider>
+  );
 }
